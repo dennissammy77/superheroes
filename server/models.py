db9eac451a18c7bc13aa24cb5b63fe197a2467f1
dennissammy77 <dennissammy77@gmail.com>
2025-06-15 15:32:00 +0300

    test (heroes): More tests for Hero MOdel

---
+++ server/models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+class Hero(db.Model):
+    __tablename__ = 'heroes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    super_name = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Hero {self.id}, {self.name} {self.super_name}>'
+
+#
+#  class Power(db.Model):
+#     pass
+
+# class HeroPower(db.Model):
+#     pass
+
--- server/testing/hero_test.py
+++ server/testing/hero_test.py
@@ -1,12 +1,36 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import app, db
 from server.models import Hero
 
-
 class TestHero:
-    '''Review model in models.py'''
+    '''Test model in models.py'''
 
     def test_can_be_instantiated(self):
         '''can be invoked to create a Python object.'''
         r = Hero()
         assert r
         assert isinstance(r, Hero)
+
+    def test_has_name(self):
+        '''can be instantiated with a name attribute.'''
+        r = Hero(name='Kamala Khan')
+        assert r.name == 'Kamala Khan'
+
+    def test_has_super_name(self):
+        '''can be instantiated with a super_name attribute.'''
+        r = Hero(super_name='Ms. Marvel')
+        assert r.super_name == 'Ms. Marvel'
+
+    def test_can_be_saved_to_database(self):
+        '''can be added to a transaction and committed to heroes table with name and super_name column.'''
+        with app.app_context():
+            assert 'name' in Hero.__table__.columns
+            assert 'super_name' in Hero.__table__.columns
+            r = Hero(name='Kamala Khan',super_name='Ms. Marvel')
+            db.session.add(r)
+            db.session.commit()
+            assert hasattr(r, 'id')
+            assert db.session.query(Hero).filter_by(id=r.id).first()
